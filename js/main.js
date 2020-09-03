e422e66582e5372abbd3587df7d81fc3946e6ac8
Nemolite <g25092011@mail.ru>
2020-09-03 08:34:29 +0300

    top-menu

--- js/main.js
+++ js/main.js
@@ -1,49 +1,15 @@
-var r1 = $("#sub1");
-$("#btn1").on("click", funk1);
+var r = $("#groups-show");
+$("#groups").on("click", funk);
 
-function funk1(){    
-	if(r1.css ("display") == "none"){
-        r1.fadeIn(100);
+function funk(){    
+	if(r.css ("display") == "none"){
+        r.fadeIn(100);
+        $("#fix-img").css("marginTop","-240px");
         }
         else{
-            r1.fadeOut(100);            
+            r.fadeOut(100); 
+            $("#fix-img").css("marginTop","0px");           
         }    
     } 
 
 
-var r2 = $("#sub2");
-$("#btn2").on("click", funk2);
-
-function funk2(){    
-	if(r2.css ("display") == "none"){
-        r2.fadeIn(100);
-        }
-        else{
-            r2.fadeOut(100);            
-        }    
-    } 
-
-var r3 = $("#sub3");
-$("#btn3").on("click", funk3);
-
-function funk3(){    
-	if(r3.css ("display") == "none"){
-        r3.fadeIn(100);
-        }
-        else{
-            r3.fadeOut(100);            
-        }    
-    }     
-
-
-var r4 = $("#sub4");
-$("#btn4").on("click", funk4);
-
-function funk4(){    
-	if(r4.css ("display") == "none"){
-        r4.fadeIn(100);
-        }
-        else{
-            r4.fadeOut(100);            
-        }    
-    }     
